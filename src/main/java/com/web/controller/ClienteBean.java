81a9091dffeba7abc1d9d3bf7285de311650552d
Luis Reino <105605562+luisreino-ch@users.noreply.github.com>
2023-03-07 12:28:39 -0500

    Cliente Bean2

--- src/main/java/com/web/controller/ClienteBean.java
+++ src/main/java/com/web/controller/ClienteBean.java
@@ -17,5 +17,52 @@ public class ClienteBean {
 	ClienteDAOImpl clienteDAO = new ClienteDAOImpl();
 	List<Cliente> listaClientes = new ArrayList<>();
 	
+	public List<Cliente> getListaClientes() {
+		return clienteDAO.obtenerClientes();
+	}
+
+	public void setListaClientes(List<Cliente> listaClientes) {
+		this.listaClientes = listaClientes;
+	}
+
+	public String editar(int id) {
+		Cliente oCliente = new Cliente();
+		oCliente = clienteDAO.buscar(id);
+		System.out.println(oCliente);
+		
+		//crear una coleccion de tipo mapa
+		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+		
+		//pasar los datos del cliente
+		sessionMap.put("cliente", oCliente);
+		
+		return "/faces/editar.xhtml";
+	}
+	
+	public String actualizar(Cliente cliente) {
+		clienteDAO.editar(cliente);
+		return "/faces/index.xhtml";
+	}
+	
+	public String eliminar(int id) {
+		clienteDAO.eliminar(id);
+		return "/faces/index.xhtml";
+	}
+	
+	public String nuevo() {
+		Cliente oCliente = new Cliente();
+		//crear una coleccion de tipo mapa
+		Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+				
+		//pasar los datos del cliente
+		sessionMap.put("cliente", oCliente);
+		return "/faces/crear.xhtml";
+	}
+	
+	public String guardar(Cliente cliente) {
+		clienteDAO.guardar(cliente);
+		return "/faces/index.xhtml";
+	}
+	
 	
 }
